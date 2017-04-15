1c470f8d50fe779a5c47a0ddeb6dfef30cbeccec
Denisov Danila <ddaLogin@gmail.com>
2017-04-16 00:05:16 +0600

    Update fast search

--- resources/assets/js/components/Search.vue
+++ resources/assets/js/components/Search.vue
@@ -1,20 +1,27 @@
 <template>
     <div>
         <form class="navbar-form navbar-left">
-            <div class="form-group">
+            <div class="form-group" id="dropdown-toggle">
                 <input v-on:keyup="search" v-model="q"  type="text" class="form-control" placeholder="Search" data-toggle="dropdown">
-                <ul class="dropdown-menu search-dropdown-menu" aria-labelledby="dropdownMenuDivider" v-if="users || posts">
-                    <!--<li v-if="users" class="dropdown-header">Users</li>-->
-                    <!--<li v-for="user in users">-->
-                        <!--<hr class="margin-0 padding-0">-->
-                        <!--<a v-bind:href="user.link" >-->
-                            <!--<img :src="user.photo_link_mini" alt="" class="photo-mini-search">-->
-                            <!--{{user.nickname}}-->
-                        <!--</a>-->
-                    <!--</li>-->
-                    <li v-if="posts" class="dropdown-header">Posts</li>
+                <ul class="dropdown-menu search-dropdown-menu" aria-labelledby="dropdownMenuDivider" v-if="typeIs('user')">
+                    <li class="dropdown-header">Users</li>
+                    <li><hr class="margin-0 padding-0"></li>
+                    <li v-for="user in users">
+                        <a v-bind:href="user.link" >
+                            <img :src="user.photo_link_mini" alt="" class="photo-mini-search">
+                            {{user.nickname}}
+                        </a>
+                    </li>
+                    <li v-if="!users" class="text-center text-warning">Users not found</li>
+                    <li class="text-center">
+                        <hr class="margin-0 padding-5">
+                        <button v-on:click.stop="setType('post')" type="button" class="btn btn-primary btn-xs">go to posts</button>
+                    </li>
+                </ul>
+                <ul class="dropdown-menu search-dropdown-menu" aria-labelledby="dropdownMenuDivider" v-if="typeIs('post')">
+                    <li class="dropdown-header">Posts</li>
+                    <li><hr class="margin-0 padding-0"></li>
                     <li v-for="post in posts">
-                        <hr class="margin-0 padding-0">
                         <a v-bind:href="post.link" >
                             <img class="photo-mini-search" :src="post.author.photo_link_mini" alt="">
                             <span v-for="tag in toJson(post.searched_tags)" class="label label-primary margin-right-4" v-html="asHashtag(tag)"></span>
@@ -22,9 +29,17 @@
                             <span v-html="post.searched_text"></span>
                         </a>
                     </li>
+                    <li v-if="!posts" class="text-center text-warning">Posts not found</li>
+                    <li class="text-center">
+                        <hr class="margin-0 padding-5">
+                        <button v-on:click.stop="setType('user')" type="button" class="btn btn-primary btn-xs">go to users</button>
+                    </li>
                 </ul>
             </div>
-            <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
+            <button type="submit" class="btn btn-default">
+                <i v-if="!loading" class="fa fa-search" aria-hidden="true"></i>
+                <i v-if="loading" class="fa fa-spinner fa-spin" aria-hidden="true"></i>
+            </button>
         </form>
     </div>
 </template>
@@ -36,18 +51,29 @@
                 q: '',
                 users: null,
                 posts: null,
+                loading: false,
+                type: 'user', //can be 'user', 'post'
             };
         },
         methods: {
             search: function () {
                 if (this.q.length >= 3){
+                    this.loading = true;
                     axios.get('/search/'+this.q).then(this.searchSuccess);
                 }
             },
             searchSuccess: function (response) {
                 this.users = (response.data.users.length)?response.data.users:null;
                 this.posts = (response.data.posts.length)?response.data.posts:null;
-                $(".form-group").addClass('open');
+                $("#dropdown-toggle").addClass('open');
+                this.loading = false;
+            },
+            typeIs: function (value) {
+                return (this.type == value);
+            },
+            setType: function (value) {
+                this.type = value;
+                $("#dropdown-toggle").addClass('open');
             },
             toJson: function (tags) {
                 var t = JSON.parse(tags);
